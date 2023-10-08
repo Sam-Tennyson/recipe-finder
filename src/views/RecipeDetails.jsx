da0af35f619fe9afb5b114f1fbd9bd70d993aef6
Saurabh-Shukla <saurabhshukla3107@gmail.com>
2023-10-08 09:25:41 +0530

    recipe-detail-page-styling

--- src/views/RecipeDetails.jsx
+++ src/views/RecipeDetails.jsx
@@ -1,5 +1,5 @@
 // libs
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 // constants
@@ -11,6 +11,10 @@ const RecipeDetails = () => {
     const navigate = useNavigate();
     const recipeDetailRed = useSelector(state => state.recipe.selectedRecipe) || null
 
+    useEffect(() => {
+        if (!recipeDetailRed) navigate(ROUTE_CONSTANTS.HOME)
+    }, [recipeDetailRed])
+
     return (
         <>
             <div className='w-4/5 mx-auto mt-20'>
@@ -18,29 +22,31 @@ const RecipeDetails = () => {
                     <h2 className='text-3xl  underline'>Recipe Information</h2>
                     <button className='p-2 bg-gray-500 rounded-2xl' onClick={() => navigate(ROUTE_CONSTANTS.HOME)}>Back</button>
                 </div>
-                <div className="grid grid-rows-3 grid-flow-col gap-4 mb-2">
-                    <div className="row-span-3 mx-auto p-2 text-center border-gray-400 rounded-md">
+                   <div className=" gap-4 mb-2">
+                    <div className="w-1/3 mx-auto border-gray-400 rounded-md mb-2">
                         <em>
                             <img src={recipeDetailRed?.image} alt="recipe-detail-image"
                                 className='object-cover text-center'
                             /> </em>
                     </div>
-                    <div className="md:col-span-2 text-xl">{recipeDetailRed?.label}</div>
-                    <div className="md:row-span-2 md:col-span-2 ">
-                        <h2>Instructions</h2>
-                        {recipeDetailRed?.ingredientLines?.map((item) => (
-                            <span className='mb-2 mr-2 block' key={item}>{item} .</span>
-                        ))}
+                    <div className='recipe-detail-class mb-2 '>
+                        <div className="text-xl underline">Title- {recipeDetailRed?.label}</div>
+                        <div className=" ">
+                            <h2 className='underline'>Instructions</h2>
+                            {recipeDetailRed?.ingredientLines?.map((item) => (
+                                <span className='mb-2 mr-2 block' key={item}>{item} .</span>
+                            ))}
+                        </div>
                     </div>
                 </div>
-                <div className="mb-2">
-                        <h2>Ingredients</h2>
-                        {recipeDetailRed?.ingredients?.map((item) => (
-                            <span className='mb-2 mr-2' key={item}>{item?.food} .</span>
-                        ))}
-                    </div>
-                <div className="mb-2">
-                    <h2>Health Labels</h2>
+                <div className="mb-2 recipe-detail-class">
+                    <h2 className='underline'>Ingredients</h2>
+                    {recipeDetailRed?.ingredients?.map((item) => (
+                        <span className='mb-2 mr-2' key={item}>{item?.food} .</span>
+                    ))}
+                </div>
+                <div className="mb-2 recipe-detail-class">
+                    <h2 className='underline'>Health Labels</h2>
                     {recipeDetailRed?.healthLabels?.map((item) => (
                         <span className='mb-2 mr-2' key={item}>{item} .</span>
                     ))}
